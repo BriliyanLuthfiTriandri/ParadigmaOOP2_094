9494202b8b294bc06230aa5a3e539eff99b4dd54
BriliyanLuthfiTriandri <briliyanlt@gmail.com>
2023-05-31 19:08:15 +0700

    menambah member constructor dan destructor pada class pelajar

--- ProgramMultiWaris/ProgramMultiWaris.cpp
+++ ProgramMultiWaris/ProgramMultiWaris.cpp
@@ -32,4 +32,17 @@ class pelajar : private manusia, public orang {
 public:
 	string sekolah;
 
+	pelajar(string pNama, string pJenisKelamin, string pSekolah) :
+		orang(pNama),
+		manusia(pJenisKelamin),
+		sekolah(pSekolah) {
+		cout << "pelajar dibuat\n" << endl;
+	}
+	~pelajar() {
+		cout << "pelajar dihapus\n" << endl;
+	}
+	string perkenalan() {
+		return "Hallo, nama saya " + nama + " dengan jenis kelamin " + jenisKelamin + " dari sekolah " + sekolah + "\n\n";
+
+	}
 };
